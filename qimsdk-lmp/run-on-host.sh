53602286537f2b6f752b282fcfe5996a30ce3a08
Andy Doan <andy@foundries.io>
2024-10-18 09:00:21 -0500

    qimsdk-lmp: Improve how we store files on host
    The new QC linux produces a `/opt/containerd` directory. Our scripts are
    colliding with this. This change organizes our files into a single
    directory so we that we can more safely do an `rm -rf` operation.
    
    Signed-off-by: Andy Doan <andy@foundries.io>

--- qimsdk-lmp/run-on-host.sh
+++ qimsdk-lmp/run-on-host.sh
@@ -1,6 +1,8 @@
 #!/bin/sh -e
 
-rm /opt/*
-cp /apps/bin/* /opt
-cp /src/models/* /opt
+rm /opt/gst -rf
+mkdir /opt/gst
+cp /apps/bin/* /opt/gst/
+cp /src/models/* /opt/gst/
+chmod +x /opt/gst/gst-*
 sshpass -p "fio" ssh -o StrictHostKeyChecking=no fio@127.0.0.1 'echo "fio" | sudo WAYLAND_USER=weston XDG_RUNTIME_DIR=/run/user/63 WAYLAND_DISPLAY=wayland-1 -S '''$@''''
