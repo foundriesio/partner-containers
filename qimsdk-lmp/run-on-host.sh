9162020d8e0ebcd6e7f10a70d9353640c4a779c7
Ricardo Salveti <ricardo@foundries.io>
2025-06-19 18:35:25 -0300

    qimsdk-lmp: update to qim sdk 1.4
    Models, labels and data extracted from
    https://raw.githubusercontent.com/quic/sample-apps-for-qualcomm-linux/refs/heads/main/download_artifacts.sh.
    
    Signed-off-by: Ricardo Salveti <ricardo@foundries.io>

--- qimsdk-lmp/run-on-host.sh
+++ qimsdk-lmp/run-on-host.sh
@@ -3,6 +3,10 @@
 rm /opt/gst -rf
 mkdir /opt/gst
 cp /apps/bin/* /opt/gst/
-cp /src/models/* /opt/gst/
+cp -rf /apps/configs /opt/gst/
+cp -rf /src/models /opt/gst/
+cp -rf /src/labels /opt/gst/
+cp -rf /src/data /opt/gst/
+cp -rf /src/media /opt/gst/
 chmod +x /opt/gst/gst-*
 sshpass -p "fio" ssh -o StrictHostKeyChecking=no fio@127.0.0.1 'echo "fio" | sudo WAYLAND_USER=weston XDG_RUNTIME_DIR=/run/user/63 WAYLAND_DISPLAY=wayland-1 -S '''$@''''
